ea28fb6905155373d2bd74d8f98b45e557266ec8
osmanuss <chahmarov.o@gmail.com>
2020-10-01 21:44:52 +0300

    Добавлено второе задание на плюсах

---
+++ Practice/02/C++/02/02/02.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    double c;
+    int a;
+    int b;
+    double d;
+    a = 3;
+    b = 3.14;
+    c = 3;
+    d = 3.14;
+    std::cout << a << std::endl << b << std::endl << c << std::endl << d << std::endl;
+}
