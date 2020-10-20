f5cf7e1e7ff70d430af4e9629a205db90df23840
osmanuss <chahmarov.o@gmail.com>
2020-10-20 15:31:00 +0300

    Добавлено: 8 C++ 5 Python

---
+++ Practice/08/C++/08/08/08.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a, b, c;
+    char d;
+    std::cin >> a >> d >> b;
+    if (d == '+')
+        c = a + b;
+    if (d == '-')
+        c = a - b;
+    if (d == '*')
+        c = a * b;
+    if (d == '/')
+        c = a / b;
+    std::cout << c;
+}
