9f0b5432e2dd049892ace8a3e473e40cdb4b4125
osmanuss <chahmarov.o@gmail.com>
2020-10-20 15:19:49 +0300

    Добавлено: 7 c++ 5 Python

---
+++ Practice/07/C++/07/07/07.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double a, b, c, s, p, a1, b1, c1, a2, b2, c2;
+    int x;
+    std::cin >> x;
+    if (x == 1)
+    {
+        std::cin >> a;
+        std::cin >> b;
+        std::cin >> c;
+        p = ((a + b + c) / 2);
+        s = sqrt(p*(p - a) * (p - b) * (p - c));
+        std::cout << "S = " << s;
+    }
+    else
+        if (x == 2)
+        {
+            std::cin >> a1 >> a2;
+            std::cin >> b1 >> b2;
+            std::cin >> c1 >> c2;
+            a = sqrt((a1 - b1) * (a1 - b1) + (a2 - b2) * (a2 - b2));
+            b = sqrt((c1 - b1) * (c1 - b1) + (c2 - b2) * (c2 - b2));
+            c = sqrt((c1 - a1) * (c1 - a1) + (c2 - a2) * (c2 - a2));
+            p = (a + b + c) / 2;
+            s = sqrt(p*(p - a) * (p - b) * (p - c));
+            std::cout << "S = " << s;
+        }
+        else
+            std::cout << "Ошибочный ввод";
+}
