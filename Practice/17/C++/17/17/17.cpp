edc943da1a488e1763acd266959a34caaf89ec50
osmanuss <chahmarov.o@gmail.com>
2020-10-30 11:06:34 +0300

    30/10/2020 11:06

--- Practice/17/C++/17/17/17.cpp
+++ Practice/17/C++/17/17/17.cpp
@@ -1,20 +1,100 @@
-﻿// 17.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
 
-#include <iostream>
+struct Roulette
+{
+	int num;
+	int color;
+	int dorp_rate;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int n = 37;
+
+	Roulette arr[n];
+	memset(arr, 0, sizeof(arr));
+
+	for (int i = 1; i < n; i++)
+		arr[i].num = i;
+
+	for (int i = 1; i < 11; i++)
+		arr[i].color = i % 2;
+
+	arr[11].color = 0;
+
+	for (int i = 12; i < 19; i++)
+		arr[i].color = (i + 1) % 2;
+
+	for (int i = 19; i < 29; i++)
+		arr[i].color = i % 2;
+
+	arr[29].color = 0;
+
+	for (int i = 30; i < n; i++)
+		arr[i].color = (i + 1) % 2;
+
+	std::vector<int> drop_rate_nums;
+	for (int i = 0; i < n; i++)
+		drop_rate_nums.push_back(i);
+
+	std::pair<int, int> drop_rate_b_r;
+	drop_rate_b_r.first = 0;
+	drop_rate_b_r.second = 0;
+
+	int most_drops = 0;
+
+	int k = 3;
+	int cin;
+
+	while (true)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			std::cin >> cin;
+			if (cin < 0)
+				return 0;
+			if (cin > 36)
+				continue;
+
+			arr[cin].dorp_rate++;
+
+			for (int i = 0; i < n; i++)
+				if (arr[i].dorp_rate > most_drops)
+					most_drops = arr[i].dorp_rate;
+
+			for (int i = 0; i < n; i++)
+				if (arr[i].dorp_rate == most_drops)
+					std::cout << arr[i].num << ' ';
+			std::cout << '\n';
+
+			for (auto i = drop_rate_nums.begin(); i < drop_rate_nums.end(); i++)
+				if (*i == cin)
+				{
+					drop_rate_nums.erase(i);
+					break;
+				}
+
+			for (auto i = drop_rate_nums.begin(); i < drop_rate_nums.end(); i++)
+				std::cout << *i << ' ';
+			std::cout << '\n';
+
+			arr[cin].color == 0 ? drop_rate_b_r.first++ : drop_rate_b_r.second++;
+
+			std::cout << drop_rate_b_r.second << ' ' << drop_rate_b_r.first << "\n\n";
+		}
+
+		for (int i = 0; i < n; i++)
+			arr[i].dorp_rate = 0;
+
+		drop_rate_nums.clear();
+		for (int i = 0; i < n; i++)
+			drop_rate_nums.push_back(i);
+
+		drop_rate_b_r.first = 0;
+		drop_rate_b_r.second = 0;
+		most_drops = 0;
+	}
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
