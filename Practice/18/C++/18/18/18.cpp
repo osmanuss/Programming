fd8f45386f0b443417ab6db66c1c8bed69665cd1
osmanuss <chahmarov.o@gmail.com>
2020-10-30 21:50:58 +0300

    Добавлено: 18 С++ 18 Python

--- Practice/18/C++/18/18/18.cpp
+++ Practice/18/C++/18/18/18.cpp
@@ -1,65 +1,33 @@
 ﻿#include <iostream>
 #include <string>
-#include <vector>
-#include <cmath>
-
-double pow(int a, int b)
-{
-	int var = 1;
-	int i = 0;
-
-	while (i++ < b)
-		var *= a;
-
-	return var;
-}
 
 int main()
 {
-	const int n = 6;
-	std::string familiarWords[n] = { "hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen" };
-
-	std::vector<char> familiarLetters;
-
-	bool exist = 1;
-
-	for (int i = 0; i < n; i++) 
-		for (int j = 0; j < familiarWords[i].length(); j++) 
-		{
-			for (auto k = familiarLetters.begin(); k < familiarLetters.end(); k++)
-				if (*k == familiarWords[i][j])
-					exist = 0;
-
-			if (exist)
-				familiarLetters.push_back(familiarWords[i][j]);
-
-			exist = 1;
-		}
-
-	std::string stopWord;
-	std::cin >> stopWord;
-
-	std::vector<bool> arr(stopWord.length());
-
-	for (int i = 0; i < stopWord.length(); i++)
-		arr[i] = 0;
-
-	for (int i = 0; i < stopWord.length(); i++)
-		for (auto k = familiarLetters.begin(); k < familiarLetters.end(); k++)
-			if (*k == stopWord[i])
-			{
-				arr[i] = 1;
-				break;
-			}
-
-	exist = 1;
-	for (int i = 0; i < stopWord.length(); i++)
-		exist *= arr[i];
-
-	if (exist)
-		std::cout << pow(double(1) / familiarLetters.size(), stopWord.length());
-	else
-		std::cout << 0;
-
-	return 0;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите слово." << std::endl;
+    std::string pas;
+    std::getline(std::cin, pas);
+    const std::string words[] = { "hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen" };
+    int chars[26] = {};
+    const int x = 'a';
+    for (const std::string& word : words)
+    {
+        for (const char& chr : word)
+        {
+            ++chars[chr - x];
+        }
+    }
+    long double chance = 1;
+    long double total = 0;
+    for (const int& item : chars)
+    {
+        total += item;
+    }
+    for (const char& chr : pas)
+    {
+        chance *= chars[chr - x] / total;
+        if (chance < LDBL_EPSILON) break;
+    }
+    std::cout.precision(16);
+    std::cout << chance << std::endl;
 }
