f0d9f16a7cb84309d4adf7f1d5b61bbe1d07a6e9
osmanuss <chahmarov.o@gmail.com>
2020-11-20 22:43:54 +0300

    Fixed 24 python

--- Practice/24/Python/24.py
+++ Practice/24/Python/24.py
@@ -1,18 +1,17 @@
 import json
-user = {}
-with open("in.json", "r") as jsif:
-    data = json.load(jsif)
-for i in data:
-    if i['userId'] in user.keys():
-        if i['completed']:
-            user[i['userId']] += 1
-    else:
-        if i['completed']:
-            user[i['userId']] = 1
-        else:
-            user[i['userId']] = 0
-result = []
-for i in user.keys():
-    result.append({"userId" : i, "task_completed" : user[i]})
-with open("out.json", "w") as jsof:
-    json.dump(result, jsof)
+with open('in.json', 'r') as file:
+    data = json.load(file)
+users = 0
+for item in data:
+    if item["userId"] > users:
+        users = item["userId"]
+complited = []
+for i in range(users):
+    complited.append(0)
+for item in data:
+    for j in range(users):
+        if item["userId"] == j and item["completed"]:
+            complited[j] = complited[j] + 1
+new_data = []
+for i in range(users):
+    new_data.append("")
