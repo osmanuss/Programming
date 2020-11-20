a63637c8e3d1145784e1c6c62573eca72dee8f03
osmanuss <chahmarov.o@gmail.com>
2020-11-20 22:32:52 +0300

    added 24 python

--- Practice/24/Python/24.py
+++ Practice/24/Python/24.py
@@ -1,46 +1,18 @@
 import json
-import os
-
-abspath = os.path.abspath(__file__)
-dirname = os.path.dirname(abspath)
-os.chdir(dirname)
-
-
-class Work:
-    def __init__(self, user_id, id, title, completed):
-        self.userId = user_id
-        self.Id = id
-        self.Title = title
-        self.Completed = completed
-
-
-def as_work(dct):
-    return Work(dct.get("userId"), dct.get("id"), dct.get("title"), dct.get("completed"))
-
-
-class DataEncoder(json.JSONEncoder):
-    def default(self, obj):
-        return obj.__dict__
-
-
-class Data:
-    def __init__(self, user_id, count=1):
-        self.task_completed = count
-        self.userId = user_id
-
-
-with open("in.json", "r") as read_file:
-    works = json.load(read_file, object_hook=as_work)
-
-    with open("out.json", "w") as write_file:
-        user_tasks = dict()
-        for work in works:
-            if not work.Completed:
-                continue
-
-            if work.userId in user_tasks:
-                user_tasks[work.userId] += 1
-            else:
-                user_tasks[work.userId] = 1
-
-        json.dump([Data(key, value) for key, value in sorted(user_tasks.items())], write_file, indent=4, cls=DataEncoder)
+user = {}
+with open("in.json", "r") as jsif:
+    data = json.load(jsif)
+for i in data:
+    if i['userId'] in user.keys():
+        if i['completed']:
+            user[i['userId']] += 1
+    else:
+        if i['completed']:
+            user[i['userId']] = 1
+        else:
+            user[i['userId']] = 0
+result = []
+for i in user.keys():
+    result.append({"userId" : i, "task_completed" : user[i]})
+with open("out.json", "w") as jsof:
+    json.dump(result, jsof)
